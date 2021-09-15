7511642a2e09b5b13e87addbecae5d87d34a6554
LewiClancy <itslewisndungu@gmail.com>
2021-09-15 20:52:17 +0300

    3 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, ShowcaseComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
