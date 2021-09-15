3790ba4ab2964ba75db345c4159cad9bff7d594b
LewiClancy <itslewisndungu@gmail.com>
2021-09-15 23:04:24 +0300

    9 files modified, 12 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ShowcaseComponent } from './showcase/showcase.component';
 import { NewsletterComponent } from './newsletter/newsletter.component';
+import { BoxesComponent } from './boxes/boxes.component';
+import { LearnComponent } from './learn/learn.component';
+import { AccordianComponent } from './accordian/accordian.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
@@ -13,7 +16,10 @@ import { NewsletterComponent } from './newsletter/newsletter.component';
     AppComponent,
     NavbarComponent,
     ShowcaseComponent,
-    NewsletterComponent
+    NewsletterComponent,
+    BoxesComponent,
+    LearnComponent,
+    AccordianComponent
   ],
   bootstrap: [AppComponent]
 })
